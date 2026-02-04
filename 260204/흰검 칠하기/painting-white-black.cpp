8faabc03f5f2a51402f75441aacd9955256a78bf
Woo <56511679+whj8119@users.noreply.github.com>
2026-02-04 15:09:26 +0900

    Title: 흰검 칠하기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260204/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int n, dirNum, pos, white, black, gray;
+int x[1000];
+char dir[1000];
+int cnt[2000000];
+const int kIndex = 1000000;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x[i] >> dir[i];
+
+        dirNum = dir[i] == 'R' ? 1 : -1;
+        cnt[pos + kIndex]++;
+        for(int j = 0; j < x[i]-1; j++)
+        {
+            pos += dirNum;
+            cnt[pos + kIndex]++;
+        }
+    }
+
+    for(int i = 0; i < kIndex; i++)
+    {
+        if (cnt[i] <= 0)
+            continue;
+        else if(cnt[i] >= 4)
+            gray++;
+        else if(cnt[i] % 2 == 1)
+            white++;
+        else
+            black++; 
+    }
+
+    for(int i = kIndex; i < kIndex*2; i++)
+    {
+        if (cnt[i] <= 0)
+            continue;
+        else if(cnt[i] >= 4)
+            gray++;
+        else if(cnt[i] % 2 == 1)
+            black++;
+        else
+            white++; 
+    }
+
+    return 0;
+}
