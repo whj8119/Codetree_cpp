65886062d5092db4d6eb3b53671e193a7e5c12d8
Woo <56511679+whj8119@users.noreply.github.com>
2026-02-08 16:01:26 +0900

    Title: 연속되는 수 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260208/연속되는 수 3/continuous-number3.cpp
+++ 260208/연속되는 수 3/continuous-number3.cpp
@@ -10,22 +10,18 @@ int main()
 {
     cin >> N;
 
-    cnt = 1;
-
     for (int i = 0; i < N; i++)
     {
         cin >> arr[i];
 
-        if(i != 0 && (arr[i] < 0 && arr[i-1] < 0 || arr[i] > 0 && arr[i-1] > 0))
+        if(i > 0 && arr[i] * arr[i-1] > 0)
             cnt++;
         else
-        {
-            maxCnt = max(cnt, maxCnt);
             cnt = 1;
-        }
+
+        maxCnt = max(cnt, maxCnt);
     }
 
-    maxCnt = max(cnt, maxCnt);
     cout << maxCnt;
 
     return 0;
